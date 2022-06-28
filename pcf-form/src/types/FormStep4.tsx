60b9ab0dd39e42ed5febaae1a310ef4f340f8af5
marcelo galli <marcelolemesgalli@hotmail.com>
2022-06-28 08:56:27 -0300

    update
    update

--- pcf-form/src/types/FormStep4.tsx
+++ pcf-form/src/types/FormStep4.tsx
@@ -1,14 +1,6 @@
 import { useCallback, useState } from "react";
 import * as SC from "../styles/styles";
 
-export interface FormStep4Input {
-  numberOfSupervisors: string;
-  averagePay: string;
-  workload: "40 horas semanais" | "30 horas semanais" | "20 horas semanais" | "Outro";
-  workloadOthers: string;
-  // supervisorQualification: string[];
-
-};
 
 export interface supervisorQualification {
   especializacao: string;
